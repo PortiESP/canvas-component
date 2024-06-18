9188d7ff44cbe53c9c55c1b3eb851870df945952
PortiESP <portiesp@gmail.com>
2024-06-18 22:45:39 +0200

    reduced delay in double click

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 export default {
     PAN_KEY: "Space",
     PAN_MOUSE_BUTTON: 1,  // Middle mouse button
-    DOUBLE_CLICK_DELAY: 400,
+    DOUBLE_CLICK_DELAY: 300,
     ZOOM_LEVELS: [0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5],
     CLEAN_MARGIN: 100,
 }
